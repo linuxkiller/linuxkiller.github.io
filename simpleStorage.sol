e2f3ee13442e81b67c3a8820c77acd1a3bd385a3
linuxkiller <linuxkiller.hoon@gmail.com>
2019-01-10 11:46:13 +0900

    Update simpleStorage.sol

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -1,7 +1,7 @@
 
 <!DOCTYPE html>
-<htmll>
-<head>
+<html>
+<headd>
   <meta charset="UTF-8">
   <meta http-equiv="CACHE-CONTROL" content="NO-CACHE">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.2.3/milligram.min.css">
